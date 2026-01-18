88040387d14506f88e3954788b57b618cf274ba8
Kakashi <karthikmotupalli34@gmail.com>
2026-01-18 13:15:06 +0530

    Delete storage-web/portfolio directory

--- storage-web/portfolio/run_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Local Web Server...
-echo.
-echo Your portfolio will be available at: http://localhost:8000
-echo Press Ctrl+C to stop the server
-echo.
-cd /d %~dp0
-python -m http.server 8000
